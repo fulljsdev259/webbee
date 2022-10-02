d84e752aa3321a457d4b29961f66d9f63f514ea6
fulljsdev259 <mohdd259@gmail.com>
2022-10-02 19:42:47 +0530

    no element on the dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { CategoryType } from "../category-type";
 
 interface DashboardInterface {}
@@ -8,12 +9,21 @@ export const Dashboard: React.FC<DashboardInterface> = () => {
   const { categories } = useSelector((state: any) => state.manageTypes);
 
   const keys = Object.keys(categories);
+
+  if (!keys.length) {
+    return (
+      <h5 className="m-3">
+        No categories available, please add from{" "}
+        <Link to="manage-types">Manage-Type</Link> section
+      </h5>
+    );
+  }
   return (
     <div>
       {keys.map((categoryId) => {
         const item = categories[categoryId];
         return (
-          <div key={categoryId} >
+          <div key={categoryId}>
             <div className="sticky-header">
               <h3> {item.categoryName.value}</h3>
             </div>
